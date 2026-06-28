ee5b0531c0ba1dfbde8398a03b98863253b53705
profsumedh-byte <prof.sumedh@gmail.com>
2026-06-28 15:33:19 +0530

    fixed neon lib

--- app/lib/prisma.js
+++ app/lib/prisma.js
@@ -1,25 +1,48 @@
+// import 'dotenv/config'
+// import { PrismaClient } from '../generated/prisma/client';
+// import { PrismaPg } from '@prisma/adapter-pg';
+// import { PrismaNeon } from '@prisma/adapter-neon';
+
+// const prismaClientSingleton = () => {
+//   const databaseUrl = process.env.DATABASE_URL;
+//   if (!databaseUrl) {
+//     throw new Error("DATABASE_URL environment variable is missing.");
+//   }
+
+//   const connectionString = process.env.DATABASE_URL;
+
+//   // const Adapter = new PrismaNeon({connectionString})
+//   // const adapter = new PrismaPg(connectionString);
+//   const Adapter = new PrismaNeon({connectionString})
+//   const adapter = new PrismaPg({Adapter});
+//   return new PrismaClient({ adapter });
+// };
+
+// const globalForPrisma = globalThis;
+
+// const prisma = globalForPrisma.prismaGlobal ?? prismaClientSingleton();
+
+// export default prisma;
+
+// if (process.env.NODE_ENV !== 'production') {
+//   globalForPrisma.prismaGlobal = prisma;
+// }
+
 import 'dotenv/config'
 import { PrismaClient } from '../generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaNeon } from '@prisma/adapter-neon';
 
 const prismaClientSingleton = () => {
-  const databaseUrl = process.env.DATABASE_URL;
-  if (!databaseUrl) {
+  const connectionString = process.env.DATABASE_URL;
+  if (!connectionString) {
     throw new Error("DATABASE_URL environment variable is missing.");
   }
 
-  const connectionString = process.env.DATABASE_URL;
-
-  // const Adapter = new PrismaNeon({connectionString})
-  // const adapter = new PrismaPg(connectionString);
-  const Adapter = new PrismaNeon({connectionString})
-  const adapter = new PrismaPg({Adapter});
+  const adapter = new PrismaNeon({ connectionString });
   return new PrismaClient({ adapter });
 };
 
 const globalForPrisma = globalThis;
-
 const prisma = globalForPrisma.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
